26721f34002284c62875ed98ad00bac0e126ff26
michaelcraig <m_craig@baylor.edu>
2018-06-25 13:43:22 -0400

    app.js and gitignore

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI); //mongodb://localhost/idea-board
+// will need to change ‘idea-board’
+// will need to change ‘idea-board’
+// will need to change ‘idea-board’
 
-var app = express();
+const connection = mongoose.connection;
+connection.on('connected', () => {
+  console.log('Mongoose Connected Successfully');
+});
+// If the connection throws an error
+connection.on('error', (err) => {
+  console.log('Mongoose default connection error: ' + err);
+});
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(express.static(__dirname + '/client/build/'));
+app.use('/api/users', usersRouter);
+app.use('/api/index', indexRouter);
+
+app.get('/*', (req,res) => {
+  res.sendFile(__dirname + '/client/build/index.html')
+})
 
 module.exports = app;
