33d01272f8d8f714d733518667e41c248a2871e3
michaelcraig <m_craig@baylor.edu>
2018-06-26 13:36:18 -0400

    All component pages linked to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import { Link, Switch, BrowserRouter as Router, Route } from 'react-router-dom'
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Login from './components/Login'
+import NewUserLogin from './components/NewUserLogin'
 import UserPage from './components/UserPage'
+import SearchResults from './components/SearchResults'
+import Playlist from './components/Playlist'
 
 class App extends Component {
   render() {
@@ -11,8 +14,11 @@ class App extends Component {
         <div>
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/login" render={Login} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/new" component={NewUserLogin} />
             <Route exact path="/user/:userId" component={UserPage} />
+            <Route exact path="/user/:userId/results" component={SearchResults} />
+            <Route exact path="/user/:userId/playlist" component={Playlist} />
           </Switch>
         </div>
       </Router>
