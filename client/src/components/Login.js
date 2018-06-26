26f9adf9c01d7fa23d8d148232bde343ffe178b7
michaelcraig <m_craig@baylor.edu>
2018-06-26 16:01:31 -0400

    very basic user page structure

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -5,6 +5,19 @@ class Login extends Component {
         return (
             <div>
                 <h1>Login Page</h1>
+                <form onSubmit="">
+                    <input
+                        type="text"
+                        placeholder='username'
+                        name='username' />
+                    <br />
+                    <input
+                        type="text"
+                        placeholder='password'
+                        name='password' />
+                    <br />
+                    <button>Login</button>
+                </form>
             </div>
         );
     }
--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -4,7 +4,19 @@ class UserPage extends Component {
     render() {
         return (
             <div>
+                <form action="">
+                    <input
+                        type="text"
+                        placeholder='Search' />
+                    <button onSearch=''>Search</button>
+                </form>
                 <h1>User Page</h1>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <hr/>
+                <h1>Playlists</h1>
             </div>
         );
     }
