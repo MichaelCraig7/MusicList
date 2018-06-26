c019db941cc2175358324113e71963c4b496ac32
michaelcraig <m_craig@baylor.edu>
2018-06-26 15:24:10 -0400

    basic homepage layout and functionality finished

--- client/src/App.js
+++ client/src/App.js
@@ -6,22 +6,35 @@ import NewUserLogin from './components/NewUserLogin'
 import UserPage from './components/UserPage'
 import SearchResults from './components/SearchResults'
 import Playlist from './components/Playlist'
+import styled from 'styled-components'
+
+const AppStyles = styled.div`
+  color: white;
+  margin: 0;
+  background-color: #252121;
+  height: 100vh;
+  h1 {
+    margin: 0;
+  }
+`
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/new" component={NewUserLogin} />
-            <Route exact path="/user/:userId" component={UserPage} />
-            <Route exact path="/user/:userId/results" component={SearchResults} />
-            <Route exact path="/user/:userId/playlist" component={Playlist} />
-          </Switch>
-        </div>
-      </Router>
+      <AppStyles>
+        <Router>
+          <div>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/new" component={NewUserLogin} />
+              <Route exact path="/user/:userId" component={UserPage} />
+              <Route exact path="/user/:userId/results" component={SearchResults} />
+              <Route exact path="/user/:userId/playlist" component={Playlist} />
+            </Switch>
+          </div>
+        </Router>
+      </AppStyles>
     );
   }
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import { Switch, BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import styled from 'styled-components'
+import Login from './Login';
+
+const HomeStyles = styled.div`
+    text-align: center;
+    button {
+        background-color: #1cd860;
+    }
+
+`
 
 class Home extends Component {
+
+
     render() {
         return (
-            <div>
-                <h1>Home Page</h1>
-            </div>
+            <HomeStyles>
+                <h1>Music List</h1>
+                <img src="http://www.iosicongallery.com/img/1024/spotify-music-2015-07-30.png" alt='' height="300" />
+                <br/>
+                <Link to="/login"><button>Enter</button></Link>
+                <Router>
+                    <Switch>
+                        <Route exact path="/login" component={Login} />
+                    </Switch>
+                </Router>
+            </HomeStyles>
         );
     }
 }
