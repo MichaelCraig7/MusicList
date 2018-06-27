711fe22c4cf24d2579c25a80f826dc1d17299b87
michaelcraig <m_craig@baylor.edu>
2018-06-27 13:44:32 -0400

    state transferred from userpage to playlist page

--- client/src/components/Playlist.js
+++ client/src/components/Playlist.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+
+
 
 class Playlist extends Component {
+
+    state = {
+        userPageState: []
+    }
+
+    componentDidMount() {
+        axios.get('/api/users').then((res) => {
+            this.setState({
+                userPageState: res.data
+            })
+        })
+    }
+
     render() {
+
+        console.log(this)
+
+
+        // const user = this.state.user
+        // const userImage = this.state.data.showUser.userImage
+        const userId = this.props.match.params.userId
+        // const userNameUrl = `/user/${userId}`
+
         return (
             <div>
+                {/* <Link to={userNameUrl}><img src={userImage} alt='' height='50' width='50' />{user}</Link> */}
                 <h1>Playlist Page</h1>
             </div>
         );
--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios'
-import { Link } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
 import styled from 'styled-components'
+import Playlist from './Playlist';
 
 
 const UserPageStyles = styled.div`
@@ -70,21 +71,28 @@ class UserPage extends Component {
     render() {
         if (this.state.data.showUser === undefined) {
             return null
-        } 
+        }
+
+        const PlaylistPayload = (props) => (
+            <Playlist
+                userPageState={this.state} {...props} />
+        )
+
         console.log(this.state.data.showUser);
         const user = this.state.user
         const userImage = this.state.data.showUser.userImage
         const userId = this.props.match.params.userId
         const userNameUrl = `/user/${userId}`
+
         return (
             <UserPageStyles>
-                <Link to={userNameUrl}><img src={userImage} alt='' height='50' width/>{user}</Link>
+                <Link to={userNameUrl}><img src={userImage} alt='' height='50' width='50' />{user}</Link>
                 <form onSubmit={this.onSubmitQuery}>
                     <input type='text' placeholder='Search' />
                     <input type='submit' value='Search' />
                 </form>
 
-                <h1><img src={userImage} alt='' height='50' width/>{user}</h1>
+                <h1><img src={userImage} alt='' height='50' width='50' />{user}</h1>
                 <br />
                 <br />
                 <br />
@@ -107,6 +115,7 @@ class UserPage extends Component {
                             </div>
                         )
                     })}
+                    <Route exact path="/user/:userId/playlist/:playlistId" render={PlaylistPayload} />
                 </div>
             </UserPageStyles>
         );
